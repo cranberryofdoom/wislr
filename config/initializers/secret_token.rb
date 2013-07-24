a53a4976a2cf9406f10787e81b97a03ebf429d1a
Ashley Qian <ashley.qian@duke.edu>
2013-07-24 16:15:25 -0700

    Initalize repository

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+Wislr::Application.config.secret_key_base = '8b89305fde7e0b46dec7ce8ce9a1e36cc68ca0b97ee3c0cbeae4b4b828b64b75cd8fb252ce4349d0c0b8b9510cb6b361daed41e3e4f6adba9599b91566ac1e6c'
